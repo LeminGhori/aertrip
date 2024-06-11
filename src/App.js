b5378be357c45f10a31ad7e354c8da4d7c09838c
LeminGhori <ghorilemin@gmail.com>
2024-06-11 16:42:34 +0530

    id

--- src/App.js
+++ src/App.js
@@ -4,8 +4,17 @@ import ErrorPage from "./pages/ErrorPage/ErrorPage.jsx";
 import Layout from "./modules/Layout/Layout.jsx";
 import Home from "./pages/Home/Home.jsx";
 import Flights from "./pages/Flights/Flights.jsx";
+import ReactGA from 'react-ga4';
+import { useEffect } from "react";
+const TRACKING_ID = "G-QQLJ0J7GDG"; // your Measurement ID
+
 
 function App() {
+  useEffect(() => {
+    ReactGA.initialize(TRACKING_ID);
+    // Send pageview with a custom path
+    ReactGA.send({ hitType: "pageview", page: "/landingpage", title: "Landing Page" });
+  }, [])
   return (
     <BrowserRouter>
       <Routes>
