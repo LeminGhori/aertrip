520a37cc83ebbb6770cfbe07b451abc5287bd0ba
LeminGhori <ghorilemin@gmail.com>
2024-06-12 09:36:32 +0530

    bug navigation

--- src/RouteChangeTracker.jsx
+++ src/RouteChangeTracker.jsx
@@ -1,22 +1,16 @@
 import React, { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import ReactGA from 'react-ga4';
 
 const TRACKING_ID = "G-QQLJ0J7GDG"; // your Measurement ID
 
 function RouteChangeTracker() {
-    const navigate = useNavigate();
+    const location = useLocation();
 
     useEffect(() => {
         ReactGA.initialize(TRACKING_ID);
-        const unlisten = navigate.listen((location) => {
-            ReactGA.send({ hitType: "pageview", page: location.pathname });
-        });
-
-        return () => {
-            unlisten();
-        };
-    }, [navigate]);
+        ReactGA.send({ hitType: "pageview", page: location.pathname });
+    }, [location]);
 
     return null;
 }
